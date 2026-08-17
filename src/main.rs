f33e92994949c797f96fc0cbb85e211779a20a8d
cleverunemployed <agonycavarly@gmail.com>
2026-08-17 22:37:40 +0400

    Character 9

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,31 @@
-// Vectors
+// panic! - критическая ошибка для выхода из программы
+// [profile.release]
+// panic = 'abort' - ускорение кода
 
-// fn main() {
-//     let mut v = vec![1, 2, 3, 4, 5];
+// Result - обработка ошибки, которая не видёт к экстреннему завершению 
 
-//     let third: &i32 = &v[2];
-//     println!("The third element is {third}");
-
-//     let third: Option<&i32> = v.get(5);
-//     match third {
-//         Some(third) => println!("The third element is {third}"),
-//         None => println!("There is no third element."),
-//     }
-
-//     for i in &mut v {
-//         *i += 50;
-//     }
-
-//     for i in &v {
-//         println!("{i}");
-//     }
+// enum Result<T, E> {
+//     Ok(T), 
+//     Err(E),
 // }
 
-// String
+    // let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+    //     if error.kind() == ErrorKind::NotFound {
+    //         File::create("hello.txt").unwrap_or_else(|error| {
+    //             panic!("Problem creating the file: {:?}", error);
+    //         })
+    //     } else {
+    //         panic!("Problem opening the file: {:?}", error);
+    //     }
+    // });
 
 
-// fn main() {
-//     for c in "Зд".chars() {
-//         println!("{c}");
-//     }
-//     for b in "Зд".bytes() {
-//         println!("{b}");
-//     }
+// ? - знак помогает для проброса ошибки, использовать при идентичной ошибке что и в Err
+// fn read_username_from_file() -> Result<String, io::Error> {
+//     let mut username_file = File::open("hello.txt")?;
+//     let mut username = String::new();
+//     username_file.read_to_string(&mut username)?;
+//     Ok(username)
 // }
 
-// HashMap
-
-// fn main() {
-//     use std::collections::HashMap;
-
-//     let text = "hello world wonderful world";
-
-//     let mut map = HashMap::new();
-
-//     for word in text.split_whitespace() {
-//         let count = map.entry(word).or_insert(0);
-//         *count += 1;
-//     }
-// {"world": 2, "wonderful": 1, "hello": 1}
-//     println!("{map:?}");
-// }
-
-
-// 1. Есть список целых чисел. Создайте функцию, используйте вектор и верните из списка: среднее значение;
-// медиану (значение элемента из середины списка после его сортировки); моду списка (mode of list, 
-// то значение которое встречается в списке наибольшее количество раз; HashMap будет полезна в данном случае).
-
-// 2. Преобразуйте строку в кодировку "поросячьей латыни" (Pig Latin). Первая согласная каждого слова 
-// перемещается в конец и к ней добавляется окончание "ay", так "first" станет "irst-fay". Слову, начинающемуся 
-// на гласную, в конец добавляется "hay" ("apple" становится "apple-hay"). Помните о деталях работы с кодировкой 
-// UTF-8!
-
-// 3.Используя хеш-карту и векторы, создайте текстовый интерфейс позволяющий пользователю добавлять имена
-// сотрудников к названию отдела компании. Например, "Add Sally to Engineering" или "Add Amir to Sales".
-// Затем позвольте пользователю получить список всех людей из отдела или всех людей в компании,
-// отсортированных по отделам в алфавитном порядке.
+// Box<dyn Error> - любая ошибка
